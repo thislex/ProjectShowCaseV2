4b593351f66a7f7716f850b0882dcc1fb7440815
Lexter Tapawan <lexter.tapawan@gmail.com>
2023-11-21 10:09:43 +0000

    Initial Commit

---
+++ ProjectShowCaseV2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectShowCaseV2
+//
+//  Created by Lexter Tapawan on 21/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectShowCaseV2/ProjectShowCaseV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectShowCaseV2App.swift
+//  ProjectShowCaseV2
+//
+//  Created by Lexter Tapawan on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectShowCaseV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
